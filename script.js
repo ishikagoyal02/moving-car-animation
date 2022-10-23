e89a5fd3995e610e71f3280aafb31b8974f148ae
Ishika Goyal <goyalishika132@gmail.com>
2022-10-23 13:39:49 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var audio = document.createElement('audio');
+audio.setAttribute('src','car audio.m4a');
+audio.loop=true;
+//audio.play();
+
+// $(document).ready(function(){
+//     $surface=$('.surface');
+//     $car=$('.car');
+
+//     $(document).on('ketpress',function(e){
+//         if(e.which==13){
+//             $($surface).toggleClass('moveRight');
+//         }
+//     })
+// });
+
+// function start()
+// {
+//     setInterval(run,100);
+
+// function run() {
+//     m+=5;
+//   var x= document.getElementById("img");
+//   x.style.marginLeft=m+'px';
+// }
+// }
